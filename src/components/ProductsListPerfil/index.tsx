98b223dd149bd7985ec7fc60112a74395c1f53f6
Kenedy Jardim <kenedydacosta@gmail.com>
2024-02-07 21:17:37 -0300

    modal criada

--- src/components/ProductsListPerfil/index.tsx
+++ src/components/ProductsListPerfil/index.tsx
@@ -1,7 +1,11 @@
 import { ContainerDefault } from '../../styles'
-import { Container, List } from './styles'
+import { Container, List, Modal, ModalContent, Prato } from './styles'
 import FoodApresentacao from '../../models/FoodApresentacao'
 import ProductPerfil from '../ProductPerfil'
+
+import close from '../../assets/images/close.svg'
+import esfiha from '../../assets/images/esfiha.svg'
+import Button from '../Button'
 
 type Props = {
   foods: FoodApresentacao[]
@@ -23,6 +27,41 @@ const ProductsListPerfil = ({ foods }: Props) => (
           />
         ))}
       </List>
+      <Modal>
+        <ContainerDefault>
+          <ModalContent>
+            <header>
+              <img src={close} alt="" />
+            </header>
+            <Prato>
+              <div>
+                <img src={esfiha} alt="" />
+              </div>
+              <div>
+                <h2>Pizza Marguerita</h2>
+                <p>
+                  A pizza Margherita é uma pizza clássica da culinária italiana,
+                  reconhecida por sua simplicidade e sabor inigualável. Ela é
+                  feita com uma base de massa fina e crocante, coberta com molho
+                  de tomate fresco, queijo mussarela de alta qualidade,
+                  manjericão fresco e azeite de oliva extra-virgem. A combinação
+                  de sabores é perfeita, com o molho de tomate suculento e
+                  ligeiramente ácido, o queijo derretido e cremoso e as folhas
+                  de manjericão frescas, que adicionam um toque de sabor
+                  herbáceo. É uma pizza simples, mas deliciosa, que agrada a
+                  todos os paladares e é uma ótima opção para qualquer ocasião.
+                </p>
+                <br />
+                <p>Serve: de 2 a 3 pessoas</p>
+                <Button to="" titleButton="">
+                  Adicionar ao carrinho - R$ 60,90
+                </Button>
+              </div>
+            </Prato>
+          </ModalContent>
+        </ContainerDefault>
+        <div className="overlay"></div>
+      </Modal>
     </ContainerDefault>
   </Container>
 )
